4640b2955980adff3cbb2f005da082f92d837b7d
Richie1811 <149514726+Richie1811@users.noreply.github.com>
2023-11-01 22:22:52 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,24 @@
+const btnVisit1=document.querySelector('.visit-1');
+btnVisit1.addEventListener('click', function() {
+    btnVisit1.textContent='Visited';
+})
+const btnVisit2=document.querySelector('.visit-2');
+btnVisit2.addEventListener('click', function() {
+    btnVisit2.textContent='Visited';
+})
+const btnVisit3=document.querySelector('.visit-3');
+btnVisit3.addEventListener('click', function() {
+    btnVisit3.textContent='Visited';
+})
+const btnVisit4=document.querySelector('.visit-4');
+btnVisit4.addEventListener('click', function() {
+    btnVisit4.textContent='Visited';
+})
+const btnVisit5=document.querySelector('.visit-5');
+btnVisit5.addEventListener('click', function() {
+    btnVisit5.textContent='Visited';
+})
+const btnVisit6=document.querySelector('.visit-6');
+btnVisit6.addEventListener('click', function() {
+    btnVisit6.textContent='Visited';
+})
